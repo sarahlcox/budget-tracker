27e71f7c4cb996c28f207b4471220689efe029be
sarahlcox <sarahl.cox.22@gmail.com>
2020-11-16 21:49:50 -0600

    updated service worker file

--- public/service-worker.js
+++ public/service-worker.js
@@ -43,9 +43,7 @@ self.addEventListener("activate", event => {
   });
 
   self.addEventListener("fetch", event => {
-    // handle runtime GET requests for data from /api routes
     if (event.request.url.includes("/api/")) {
-      // make network request and fallback to cache if network request fails (offline)
       event.respondWith(
         caches.open(RUNTIME_CACHE).then(cache => {
           return fetch(event.request)
@@ -59,14 +57,12 @@ self.addEventListener("activate", event => {
       return;
     }
   
-    // use cache first for all other requests for performance
     event.respondWith(
       caches.match(event.request).then(cachedResponse => {
         if (cachedResponse) {
           return cachedResponse;
         }
   
-        // request is not in cache. make network request and cache the response
         return caches.open(RUNTIME_CACHE).then(cache => {
           return fetch(event.request).then(response => {
             return cache.put(event.request, response.clone()).then(() => {
